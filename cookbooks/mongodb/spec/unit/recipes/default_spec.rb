8c3732ada413b7dafabdb9795cd9a6cdf4d07cf7
simon2e <simon@tuohey.net>
2017-04-24 22:20:58 +1000

    fixed up inspec files

--- cookbooks/mongodb/spec/unit/recipes/default_spec.rb
+++ cookbooks/mongodb/spec/unit/recipes/default_spec.rb
@@ -7,12 +7,12 @@
 require 'spec_helper'
 
 describe 'mongodb::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on a Centos 7.3.1611' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
+            runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
--- cookbooks/mongodb/spec/unit/recipes/mongodb_repo_spec.rb
+++ cookbooks/mongodb/spec/unit/recipes/mongodb_repo_spec.rb
@@ -1,18 +1,18 @@
 #
 # Cookbook:: mongodb
-# Spec:: default
+# Spec:: mongodb_repo
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'mongodb::mongosysconfig' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+describe 'mongodb::mongodb_repo' do
+  context 'When all attributes are default, on a Centos 7.3.1611' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
+            runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
