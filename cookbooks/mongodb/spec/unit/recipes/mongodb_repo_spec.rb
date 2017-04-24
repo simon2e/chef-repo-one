2b0eb69f075d40b0a2db81201442553873f95889
simon2e <simon@tuohey.net>
2017-04-24 22:40:58 +1000

    ready for recipe dev

--- cookbooks/mongodb/spec/unit/recipes/mongodb_repo_spec.rb
+++ cookbooks/mongodb/spec/unit/recipes/mongodb_repo_spec.rb
@@ -9,10 +9,8 @@ require 'spec_helper'
 describe 'mongodb::mongodb_repo' do
   context 'When all attributes are default, on a Centos 7.3.1611' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
-            runner.converge(described_recipe)
+      runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
--- cookbooks/mongodb/spec/unit/recipes/mongodb_sys_config_spec.rb
+++ cookbooks/mongodb/spec/unit/recipes/mongodb_sys_config_spec.rb
@@ -7,11 +7,9 @@
 require 'spec_helper'
 
 describe 'mongodb::mongodb_sys_config' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on a Centos 7.3.1611' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
       runner.converge(described_recipe)
     end
 
