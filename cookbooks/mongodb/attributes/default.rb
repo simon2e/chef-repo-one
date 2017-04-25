c41cc558a781315467a8fca62b075ef6dc059c52
simon2e <simon@tuohey.net>
2017-04-25 18:20:41 +1000

    adding sys_config recipe

--- cookbooks/mongodb/attributes/default.rb
+++ cookbooks/mongodb/attributes/default.rb
@@ -9,7 +9,7 @@ default['mongodb_yum_repo']['mongodb_version'] = '3.4'
 # major server config items. Port and IP should probably be overridden for production role
 # if the db, log and pidfile paths are changed they will need to be provisioned
 default['mongodb']['config']['systemLogPath']  = '/var/log/mongodb/mongod.log'
-default['mongodb']['config']['dbPath']         = '/var/lib/mongod'
+default['mongodb']['config']['dbPath']         = '/var/lib/mongo'
 default['mongodb']['config']['pidFilePath']    = '/var/run/mongodb/mongod.pid'
 default['mongodb']['config']['netPort']        = '27017'    # default is 27017
 default['mongodb']['config']['bindIp']         = '0.0.0.0'
--- cookbooks/mongodb/recipes/mongodb_sys_config.rb
+++ cookbooks/mongodb/recipes/mongodb_sys_config.rb
@@ -3,3 +3,11 @@
 # Recipe:: mongodb_sys_config
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+cookbook 'limits', '~> 1.0.0'
+
+set_limit 'mongod' do
+  type 'soft'
+  item 'nproc'
+  value 32000
+end
