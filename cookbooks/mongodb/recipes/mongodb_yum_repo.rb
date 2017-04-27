22047e08ca58ffcfcbb48c54b441482972c8726f
simon2e <simon@tuohey.net>
2017-04-27 21:55:49 +1000

    added version to yum repo description

--- cookbooks/mongodb/recipes/mongodb_yum_repo.rb
+++ cookbooks/mongodb/recipes/mongodb_yum_repo.rb
@@ -9,7 +9,7 @@
 repover= node['mongodb_yum_repo']['mongodb_version']
 
 yum_repository "mongodb-org-#{repover}" do
-  description "MongoDB Repository - "
+  description "MongoDB Repository - #{repover}"
   baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/#{repover}/$basearch/"
   gpgkey "https://www.mongodb.org/static/pgp/server-#{repover}.asc"
   action :create
